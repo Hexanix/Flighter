c72dda76dc80264ac2bf0dc61b160751cb28d3f7
Hexanix <damyan.dichev@gmail.com>
2020-08-03 20:52:35 +0300

    Friction_Handle function added
    Added a function to handle friction.
    Due to this, there is a velocityToAdd function additionally to Velocity

--- Scripts/PlayerBody.gd
+++ Scripts/PlayerBody.gd
@@ -2,14 +2,23 @@ extends KinematicBody2D
 
 #Velocity Vars#
 var velocity = Vector2(0, 0)
+var velocityToAdd = Vector2(0, 0)
 var velocity_HorLimit = Vector2(0, 0)
 var velocity_VerLimit = Vector2(0, 0)
 
-export var acceleration = 4
-export var jumpPower = 0.7
-export var flapPower = 0.2
-export var frict = 2
-export var gravity = 20
+#Character Variables
+export var acceleration = 1
+export var dashPower = 2.5
+
+export var jumpPower = 2
+export var flapPower = 1.8
+
+var speedHorizontal = 5000
+var speedVertical = 100000
+
+#World Variables
+export var frict = 0.6
+export var gravity = 2.3
 
 
 #States of the player, both variants#
@@ -18,6 +27,7 @@ enum stateMovement{
 	idle_ground, 
 	moving_air, 
 	moving_ground, 
+	wall_cling
 	dash
 }
 enum stateAction{
@@ -30,105 +40,155 @@ enum stateAction{
 var currentMoveState
 var currentActionState
 
-#Custom functions
-
-func input_handle():
-	
-	#Check state and determine calculations
-	
-	if currentMoveState == stateMovement.idle_ground:
-		if Input.is_action_pressed("ui_right"):
-			velocity.x += acceleration
-		elif velocity.x > 0:
-			velocity.x -= frict
-			velocity.x = clamp(velocity.x, 0, velocity_HorLimit.y)
-				
-		if Input.is_action_pressed("ui_left"):
-			velocity.x -= acceleration
-		elif velocity.x < 0:
-			velocity.x += frict
-			velocity.x = clamp(velocity.x, velocity_HorLimit.x, 0)
-		
-		if Input.is_action_just_pressed("wingflap"):
-			if currentMoveState == stateMovement.idle_ground:
-				velocity.y += jumpPower
-				currentMoveState = stateMovement.idle_air
-				print(velocity.y)
-	#			
-	if currentMoveState == stateMovement.idle_air:
-		
-		if Input.is_action_pressed("ui_right"):
-			velocity.x += acceleration
-		elif velocity.x > 0:
-			velocity.x -= frict
-			velocity.x = clamp(velocity.x, 0, velocity_HorLimit.y)
-				
-		if Input.is_action_pressed("ui_left"):
-			velocity.x -= acceleration
-		elif velocity.x < 0:
-			velocity.x += frict
-			velocity.x = clamp(velocity.x, velocity_HorLimit.x, 0)
-		
-		if Input.is_action_just_pressed("wingflap"):
-			if currentMoveState == stateMovement.idle_ground:
-				velocity.y += flapPower
-				print(velocity.y)
-	
-		
-	pass
-	
-#Checks in proccess if states should be changed
-func state_handle():
-	
-	
-	pass
-
 #System functions
-
 func _ready():
 	#Movement units are multiplied by 1000 to use with delta#
-	acceleration *= 1000
-	frict *= 1000
-	jumpPower *= -10000*gravity
-	gravity *= 10000
+	acceleration *= speedHorizontal
+	frict *= speedHorizontal
+	dashPower *= speedHorizontal
+	
+	jumpPower *= gravity/1.4
+	jumpPower *= -speedVertical
+	
+	
+	flapPower *= -speedVertical
+	flapPower -= gravity
+	
+	gravity *= speedVertical/10
+	
 	
 	
 	#Velocity initialized#
 	velocity_HorLimit = Vector2(acceleration*-8,acceleration*8);
-	velocity_VerLimit = Vector2(-gravity*20, gravity*3)
+	velocity_VerLimit = Vector2(-gravity*200, gravity*3)
 	velocity = Vector2(0, 0)
+	
 	
 	#We start midair#
 	currentMoveState = stateMovement.idle_air
 	currentActionState = stateAction.neutral
 	pass
 
-func _process(delta):
-	#Input handler event
-	input_handle()
+#Custom functions
+func input_handle():
+	#Check state and determine calculations
+	
+	#IDLE_GROUND
+	if currentMoveState == stateMovement.idle_ground or currentMoveState == stateMovement.moving_ground:
+		if Input.is_action_pressed("ui_right"):
+			velocityToAdd.x += acceleration
+				
+		if Input.is_action_pressed("ui_left"):
+			velocityToAdd.x -= acceleration
+		
+		if Input.is_action_just_pressed("wingflap"):
+			velocityToAdd.y += jumpPower
+				
+	#IDLE_AIR
+	elif currentMoveState == stateMovement.idle_air or currentMoveState == stateMovement.moving_air:
+		
+		if Input.is_action_pressed("ui_right"):
+			velocityToAdd.x += acceleration
+				
+		if Input.is_action_pressed("ui_left"):
+			velocityToAdd.x -= acceleration
+		
+		if Input.is_action_just_pressed("wingflap"):
+				velocityToAdd.y = 0
+				velocityToAdd.y += flapPower       
+		
+	#Regulate speed achievable via normal movement
+	velocityToAdd.x = clamp(velocityToAdd.x, velocity_HorLimit.x, velocity_HorLimit.y)                                                                 
+	
+	#DASH 
+	if Input.is_action_just_pressed("dash"):
+		print("a")
+		if Input.is_action_pressed("ui_left"):
+			velocityToAdd.x -= dashPower
+		elif Input.is_action_pressed("ui_right"):
+			velocityToAdd.x += dashPower
+		
 	pass
 	
-func _physics_process(delta):
+#Checks in proccess if states should be changed
+func state_handle():
 	
-	velocity.y += gravity
+	#If it is not changed, it stays in the air.
+	if velocity.x != 0:
+		currentMoveState = stateMovement.moving_air
+	else:
+		currentMoveState = stateMovement.idle_air
+		pass
 	
-	#Placeholder State management
-	
-	currentMoveState = stateMovement.idle_air
+	#Check if colliding with any terrain
 	for i in range(get_slide_count()):
 		var currentCollider = get_slide_collision(i).get_collider()
 		
 		if currentCollider.is_in_group("ground"):
-			currentMoveState = stateMovement.idle_ground
-			velocity.y = 0
-			break
-			pass 
+			if velocity.x == 0:
+				currentMoveState = stateMovement.idle_ground
+				return
+				pass
+			else:
+				currentMoveState = stateMovement.moving_ground
+				return
+				pass
+		elif currentCollider.is_in_group("wall"):
+			currentMoveState = stateMovement.wall_cling
+			return
+			pass
 		pass
+		#If nothing returns, obviously mid-air.
+	pass
+
+#Friction handle
+func friction_handle():
+	if velocity.x > 0:
+		velocityToAdd.x -= frict
+		if velocity.x - frict < 0:
+			velocityToAdd.x  = -velocity.x
+	elif velocity.x < 0:
+		velocityToAdd.x  += frict
+		if velocity.x + frict > 0:
+			velocityToAdd.x  = -velocity.x
 	
+	pass
+		
+
+	
+	print(velocityToAdd)
+
+func _physics_process(delta):
+	
+	#Add Gravity
+	velocity.y += gravity
+	
+	#Input handler event
+	input_handle()
+	
+	#State handler
+	state_handle()
+	
+	#Handle friction via ToAdd
+	friction_handle()
+	
+	#TODO: Clamp the velocity when added
+	
+	#Regulate velocity
+
+	velocity.x += velocityToAdd.x
+	velocity.y += velocityToAdd.y
+	
+	#Write custom function, no clamp, so you can dash.
 	velocity.y = clamp(velocity.y, velocity_VerLimit.x, velocity_VerLimit.y)
 	velocity.x = clamp(velocity.x, velocity_HorLimit.x, velocity_HorLimit.y)
 
-	print(currentMoveState)
+	#Move 
 	move_and_slide(velocity*delta)
+	
+	#Reset velToAdd variable
+	velocityToAdd.x = 0
+	velocityToAdd.y = 0
+
 	
 	pass
